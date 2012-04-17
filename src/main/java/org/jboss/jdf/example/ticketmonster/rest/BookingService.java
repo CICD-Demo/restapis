d130aca4f7a85998e8ef8b38b6b2e5606b5a8730
Pete Muir <pmuir@bleepbleep.org.uk>
2012-04-17 16:06:24 +0530

    Javadoc for BookingService

--- src/main/java/org/jboss/jdf/example/ticketmonster/rest/BookingService.java
+++ src/main/java/org/jboss/jdf/example/ticketmonster/rest/BookingService.java
@@ -31,9 +31,11 @@ import org.jboss.jdf.example.ticketmonster.model.TicketCategory;
 import org.jboss.jdf.example.ticketmonster.model.TicketPriceCategory;
 import org.jboss.jdf.example.ticketmonster.service.SeatAllocationService;
 
-
 /**
+ * JAX-RS endpoint that handles bookings
+ * 
  * @author Marius Bogoevici
+ * @author Pete Muir
  */
 @Path("/bookings")
 @Stateful
@@ -44,7 +46,7 @@ public class BookingService extends BaseEntityService<Booking> {
     SeatAllocationService seatAllocationService;
 
     public BookingService() {
-        super(Booking.class);    //To change body of overridden methods use File | Settings | File Templates.
+        super(Booking.class);
     }
 
     @DELETE
@@ -63,8 +65,9 @@ public class BookingService extends BaseEntityService<Booking> {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createBooking(BookingRequest bookingRequest) {
         try {
-            Performance performance = getEntityManager().find(Performance.class, bookingRequest.getPerformance());
 
+            // First, validate the posted data
+            // There will be more validation when persistence occurs
             Set<Long> priceCategoryIds = new HashSet<Long>();
             for (TicketRequest ticketRequest : bookingRequest.getTicketRequests()) {
                 if (priceCategoryIds.contains(ticketRequest.getPriceCategory())) {
@@ -72,50 +75,72 @@ public class BookingService extends BaseEntityService<Booking> {
                 }
                 priceCategoryIds.add(ticketRequest.getPriceCategory());
             }
-            List<TicketPriceCategory> ticketPrices = (List<TicketPriceCategory>) getEntityManager().createQuery("select p from TicketPriceCategory p where p.id in :ids").setParameter("ids", priceCategoryIds).getResultList();
 
-            Map<Long, TicketPriceCategory> priceCategoriesById = new HashMap<Long, TicketPriceCategory>();
+            // First, load the entities that make up this booking's relationships
+            Performance performance = getEntityManager().find(Performance.class, bookingRequest.getPerformance());
 
-            for (TicketPriceCategory loadedPriceCategory : ticketPrices) {
-                priceCategoriesById.put(loadedPriceCategory.getId(), loadedPriceCategory);
+            // As we can have a mix of ticket types in a booking, we need to load all of them that are relevant, and map them by
+            // id
+            List<TicketPriceCategory> ticketPrices = (List<TicketPriceCategory>) getEntityManager()
+                    .createQuery("select p from TicketPriceCategory p where p.id in :ids")
+                    .setParameter("ids", priceCategoryIds).getResultList();
+            // Now, map them by id
+            Map<Long, TicketPriceCategory> ticketPricesById = new HashMap<Long, TicketPriceCategory>();
+            for (TicketPriceCategory ticketPrice : ticketPrices) {
+                ticketPricesById.put(ticketPrice.getId(), ticketPrice);
             }
 
+            // Now, start to create the booking from the posted data
+            // Set the simple stuff first!
             Booking booking = new Booking();
             booking.setContactEmail(bookingRequest.getEmail());
+            booking.setPerformance(performance);
+            booking.setCancellationCode("abc");
 
+            // Now, we iterate over each ticket that was requested, and organize them by section and category
             Map<Section, Map<TicketCategory, TicketRequest>> ticketRequestsPerSection = new LinkedHashMap<Section, Map<TicketCategory, TicketRequest>>();
             for (TicketRequest ticketRequest : bookingRequest.getTicketRequests()) {
-                final TicketPriceCategory priceCategory = priceCategoriesById.get(ticketRequest.getPriceCategory());
+                final TicketPriceCategory priceCategory = ticketPricesById.get(ticketRequest.getPriceCategory());
                 if (!ticketRequestsPerSection.containsKey(priceCategory.getSection())) {
-                    ticketRequestsPerSection.put(priceCategory.getSection(), new LinkedHashMap<TicketCategory, TicketRequest>());
+                    ticketRequestsPerSection
+                            .put(priceCategory.getSection(), new LinkedHashMap<TicketCategory, TicketRequest>());
                 }
-                ticketRequestsPerSection.get(priceCategory.getSection()).put(priceCategoriesById.get(ticketRequest.getPriceCategory()).getTicketCategory(), ticketRequest);
+                ticketRequestsPerSection.get(priceCategory.getSection()).put(
+                        ticketPricesById.get(ticketRequest.getPriceCategory()).getTicketCategory(), ticketRequest);
             }
 
+            // Now, we can allocate the tickets
+            // Iterate over the sections
             for (Section section : ticketRequestsPerSection.keySet()) {
                 int totalTicketsRequestedPerSection = 0;
+                // Compute the total number of tickets required (a ticket category doesn't impact the actual seat!)
                 final Map<TicketCategory, TicketRequest> ticketRequestsByCategories = ticketRequestsPerSection.get(section);
                 for (TicketRequest ticketRequest : ticketRequestsByCategories.values()) {
                     totalTicketsRequestedPerSection += ticketRequest.getQuantity();
                 }
-                List<Seat> seats = seatAllocationService.allocateSeats(section, performance, totalTicketsRequestedPerSection, true);
+                // Allocate the seats
+                List<Seat> seats = seatAllocationService.allocateSeats(section, performance, totalTicketsRequestedPerSection,
+                        true);
                 int seatCounter = 0;
+                // Now, add a ticket for each requested ticket to the booking
                 for (TicketCategory ticketCategory : ticketRequestsByCategories.keySet()) {
                     final TicketRequest ticketRequest = ticketRequestsByCategories.get(ticketCategory);
-                    final TicketPriceCategory ticketPriceCategory = priceCategoriesById.get(ticketRequest.getPriceCategory());
+                    final TicketPriceCategory ticketPriceCategory = ticketPricesById.get(ticketRequest.getPriceCategory());
                     for (int i = 0; i < ticketRequest.getQuantity(); i++) {
                         Ticket ticket = new Ticket(seats.get(seatCounter + i), ticketCategory, ticketPriceCategory.getPrice());
-                        getEntityManager().persist(ticket);
+                        // getEntityManager().persist(ticket);
                         booking.getTickets().add(ticket);
                     }
                     seatCounter += ticketRequest.getQuantity();
                 }
             }
-            booking.setPerformance(performance);
-            booking.setCancellationCode("abc");
+
+            // Persist the booking, including cascaded relationships
             getEntityManager().persist(booking);
+            // And finally, tell the app we succeeded
             return Response.ok().entity(booking).type(MediaType.APPLICATION_JSON_TYPE).build();
         } catch (ConstraintViolationException e) {
+            // If validation of the data failed using Bean Validation, then send an error
             Map<String, Object> errors = new HashMap<String, Object>();
             List<String> errorMessages = new ArrayList<String>();
             for (ConstraintViolation<?> constraintViolation : e.getConstraintViolations()) {
@@ -124,12 +149,21 @@ public class BookingService extends BaseEntityService<Booking> {
             errors.put("errors", errorMessages);
             return Response.status(Response.Status.BAD_REQUEST).entity(errors).build();
         } catch (Exception e) {
+            // Finally, handle unexpected exceptions
             Map<String, Object> errors = new HashMap<String, Object>();
             errors.put("errors", Collections.singletonList(e.getMessage()));
             return Response.status(Response.Status.BAD_REQUEST).entity(errors).build();
         }
     }
 
+    /**
+     * A {@link BookingRequest} is populated with unmarshalled JSON data, and handed to @{link
+     * {@link BookingService#createBooking(BookingRequest)}.
+     * 
+     * @author Marius Bogoevici
+     * @author Pete Muir
+     * 
+     */
     public static class BookingRequest {
 
         private List<TicketRequest> ticketRequests = new ArrayList<TicketRequest>();
@@ -162,13 +196,18 @@ public class BookingService extends BaseEntityService<Booking> {
         }
     }
 
-
+    /**
+     * A {@link BookingRequest} will contain multiple {@link TicketRequest}s.
+     * 
+     * @author Marius Bogoevici
+     * @author Pete Muir
+     * 
+     */
     public static class TicketRequest {
 
         private long priceCategory;
 
         private int quantity;
-
 
         public long getPriceCategory() {
             return priceCategory;
