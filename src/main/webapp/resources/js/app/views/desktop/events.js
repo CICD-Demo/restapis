bd2b616a3bff1b7898aefcfeb62c6ed91cf42d20
Marius Bogoevici <marius.bogoevici@gmail.com>
2012-05-03 17:00:22 -0400

    Simplification of events view

--- src/main/webapp/resources/js/app/views/desktop/events.js
+++ src/main/webapp/resources/js/app/views/desktop/events.js
@@ -1,39 +1,22 @@
 define([
     'backbone',
     'utilities',
-    'text!../../../../templates/desktop/category-title.html',
-    'text!../../../../templates/desktop/event-summary.html',
-    'text!../../../../templates/desktop/event-carousel.html',
-    'text!../../../../templates/desktop/main-view.html',
+    'text!../../../../templates/desktop/events.html',
     'backbone'
 ], function (
     Backbone,
     utilities,
-    categoryTitleTemplate,
-    eventSummaryTemplate,
-    eventCarouselTemplate,
-    mainViewTemplate) {
+    eventsTemplate) {
 
-    var EventMenuView = Backbone.View.extend({
+    var EventsView = Backbone.View.extend({
         events:{
             "click a":"update"
         },
-        tagName:'div',
         render:function () {
-            var self = this
-            $(this.el).empty()
-            var current_category = null
-            _.each(this.model.models, function (event) {
-                var model_category = event.get('category')
-                if (current_category !== model_category.id) {
-                    $(self.el).append(utilities.renderTemplate(categoryTitleTemplate, model_category));
-                    current_category = model_category.id;
-                }
-                var view = new EventSummaryLineView({summaryView:self.options.summaryView, model:event});
-                $("#category-" + current_category).append(view.render().el);
-            })
+            utilities.applyTemplate($(this.el), eventsTemplate, {model:this.model})
+            $(this.el).find('.item:first').addClass('active');
             $(".collapse").collapse()
-            $("a[rel='popover']").popover({trigger:'hover'})
+            $("a[rel='popover']").popover({trigger:'hover'});
             return this
         },
         update:function () {
@@ -41,36 +24,5 @@ define([
         }
     });
 
-    var EventSummaryLineView = Backbone.View.extend({
-        tagName:'div',
-        events:{
-            "click":"notify"
-        },
-        render:function () {
-            utilities.applyTemplate($(this.el), eventSummaryTemplate, this.model.attributes)
-            return this;
-        },
-        notify:function () {
-            this.options.summaryView.render(this.model)
-        }
-    });
-
-    var EventSummaryView = Backbone.View.extend({
-        render:function () {
-            utilities.applyTemplate($(this.el), eventCarouselTemplate, {models:this.model.models});
-            $(this.el).find('.item:first').addClass('active');
-            return this;
-        }
-    });
-
-
-    return  Backbone.View.extend({
-        render:function () {
-            utilities.applyTemplate($(this.el), mainViewTemplate, {})
-            var summaryView = new EventSummaryView({model:this.model});
-            $("#itemSummary").append(summaryView.render().el)
-            this.menuView = new EventMenuView({summaryView:summaryView, model:this.model, el:$("#itemMenu")});
-            this.menuView.render()
-        }
-    });
+    return  EventsView;
 });
