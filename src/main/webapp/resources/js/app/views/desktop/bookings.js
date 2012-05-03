d2028645ebf2a47d2cb17a409da7699d7f79d8e2
Marius Bogoevici <marius.bogoevici@gmail.com>
2012-05-03 16:26:19 -0400

    Simplification - collapse bookings in a single view

--- src/main/webapp/resources/js/app/views/desktop/bookings.js
+++ src/main/webapp/resources/js/app/views/desktop/bookings.js
@@ -1,48 +1,23 @@
 define([
     'backbone',
     'utilities',
-    'text!../../../../templates/desktop/booking-row.html',
-    'text!../../../../templates/desktop/booking-table.html',
-    'require',
-    'bootstrap'
+    'text!../../../../templates/desktop/booking-table.html'
 ],function (Backbone,
             utilities,
-            bookingRowTemplate,
-            bookingTableTemplate,
-            require) {
-
-
-    // A subview of the BookingsView
-    var BookingRowView = Backbone.View.extend({
-        tagName:'tr',
-        events:{
-            "click i[data-tm-role='delete']":"deleteBooking",
-            "click a":"showDetails"
-        },
-        render:function () {
-            utilities.applyTemplate($(this.el), bookingRowTemplate, this.model.attributes);
-            return this;
-        },
-        deleteBooking:function (event) {
-            if (confirm("Are you sure you want to delete booking " + this.model.get('id'))) {
-                this.model.destroy({wait:true});
-            }
-            event.stopPropagation();
-            event.stopImmediatePropagation();
-        },
-        showDetails:function () {
-            require('router').navigate("#bookings/" + this.model.get('id'), true);
-        }
-    });
-
+            bookingTableTemplate) {
 
     var BookingsView = Backbone.View.extend({
+        events:{
+            "click i[data-tm-role='delete']":"deleteBooking"
+        },
         render:function () {
-            utilities.applyTemplate($(this.el), bookingTableTemplate, {});
-            _.each(this.model.models, function (booking) {
-                var bookingView = new BookingRowView({model:booking});
-                $("#bookingList").append(bookingView.render().el);
-            });
+            utilities.applyTemplate($(this.el), bookingTableTemplate, {model:this.model});
+        },
+        deleteBooking:function (event) {
+            var id = $(event.currentTarget).data("tm-id");
+            if (confirm("Are you sure you want to delete booking " + this.model.get(id).get('id'))) {
+                this.model.get(id).destroy({wait:true});
+            };
         }
     });
 
