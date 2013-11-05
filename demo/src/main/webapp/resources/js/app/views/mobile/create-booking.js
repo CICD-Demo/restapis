165c321dc5dc6e577ec6be9782b44c958427edc3
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2013-11-05 08:32:00 +0530

    JDF-552 Corrected error handling during booking for mobile site.
    Human readable error messages are parsed and displayed when they
    are available in the HTTP response. Ticket allocations are still
    validated on the server-side since they can fail.
    
    Additionally, JS validation is added in the Create Booking screen
    to ensure that only valid email addresses and valid ticket
    quantities enable the checkout button. Field-level messages are
    displayed when the validation fails.

--- demo/src/main/webapp/resources/js/app/views/mobile/create-booking.js
+++ demo/src/main/webapp/resources/js/app/views/mobile/create-booking.js
@@ -143,9 +143,56 @@ define([
             this.render();
         },
         updateForm:function () {
+            var valid = true;
             this.model.email = $("input[type='email']").val();
+            $("input[type='number']").each(function(idx,element) {
+                var quantity = $(this).val();
+                if(!$.isNumeric(quantity)  // is a non-number, other than empty string
+                        || quantity <= 0 // is negative
+                        || parseFloat(quantity) != parseInt(quantity)) {
+                    $("#error-" + element.id).empty().append("Should be a positive number.");
+                    $('a[id="confirmBooking"]').removeClass('ui-disabled');
+                    valid = false;
+                } else {
+                    $("#error-" + element.id).empty();
+                    $('a[id="confirmBooking"]').addClass('ui-disabled');
+                }
+            });
+            try {
+                var validElements = document.querySelectorAll(":valid");
+                var $email = $("#email");
+                var emailElem = $email.get(0);
+                var validEmail = false;
+                for (var ctr=0; ctr < validElements.length; ctr++) {
+                    if (emailElem === validElements[ctr]) {
+                        validEmail = true;
+                    }
+                }
+                if(validEmail) {
+                    this.model.email = $email.val();
+                    $("#error-email").empty();
+                } else {
+                    $("#error-email").empty().append("Please enter a valid e-mail address");
+                    delete this.model.email;
+                    valid = false;
+                }
+            }
+            catch(e) {
+                // For browsers like IE9 that do fail on querySelectorAll for CSS pseudo selectors,
+                // we use the regex defined in the HTML5 spec.
+                var emailRegex = new RegExp("[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*");
+                var emailValue = $("#email").val();
+                if(emailRegex.test(emailValue)) {
+                    this.model.email = emailValue;
+                    $("#error-email").empty();
+                } else {
+                    $("#error-email").empty().append("Please enter a valid e-mail address");
+                    delete this.model.email;
+                    valid = false;
+                }
+            }
             var totals = this.computeTotals(this.ticketCategoriesView.model);
-            if (totals.tickets > 0 && $("input[type='email']").val()) {
+            if (totals.tickets > 0 && valid) {
                 $('a[id="confirmBooking"]').removeClass('ui-disabled');
             } else {
                 $('a[id="confirmBooking"]').addClass('ui-disabled');
@@ -199,7 +246,21 @@ define([
                     utilities.applyTemplate($(self.el), bookingDetailsTemplate, booking);
                     $(self.el).trigger('pagecreate');
                 }}).error(function (error) {
-                    alert(error);
+                    try {
+                        var response = JSON.parse(error.responseText);
+                        var displayMessage = "";
+                        if(response && response.errors) {
+                            var errors = response.errors;
+                            for(var idx = 0; idx < errors.length; idx++) {
+                                displayMessage += errors[idx] + "\n";
+                            }
+                            alert(displayMessage);
+                        } else {
+                            alert("Failed to perform the bookng.");
+                        }
+                    } catch (e) {
+                        alert("Failed to perform the bookng.");
+                    }
                 });
         },
         deleteBooking: function(event) {
@@ -215,9 +276,9 @@ define([
             if($("#sectionSelect").length) {
                 var self = this;
                 $("input").each( function(index,element) {
-                    if(element.value !== self.formValues[element.name]) {
-                        self.formValues[element.name] = element.value;
-                        $("input[name='"+element.name+"']").change();
+                    if(element.value !== self.formValues[element.id]) {
+                        self.formValues[element.id] = element.value;
+                        $("input[id='"+element.id+"']").change();
                     }
                 });
                 this.timerObject = setTimeout(function() {
