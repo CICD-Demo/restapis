0f836ab80fec28cb9144921ec251b40a11e175ea
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2013-11-12 13:12:09 +0530

    JDF-416 Updated the tutorial for ShrinkWrap deployment updates
    Updated the tutorial to incrementally update the ShrinkWrap
    deployment used in the test suite.

--- demo/src/test/java/org/jboss/jdf/ticketmonster/test/rest/RESTDeployment.java
+++ demo/src/test/java/org/jboss/jdf/ticketmonster/test/rest/RESTDeployment.java
@@ -3,10 +3,9 @@ package org.jboss.jdf.ticketmonster.test.rest;
 import org.jboss.jdf.example.ticketmonster.model.Booking;
 import org.jboss.jdf.example.ticketmonster.model.search.PriceMinBridge;
 import org.jboss.jdf.example.ticketmonster.rest.BaseEntityService;
-import org.jboss.jdf.example.ticketmonster.rest.CartService;
 import org.jboss.jdf.example.ticketmonster.rest.dto.VenueDTO;
 import org.jboss.jdf.example.ticketmonster.rest.search.SearchService;
-import org.jboss.jdf.example.ticketmonster.service.*;
+import org.jboss.jdf.example.ticketmonster.service.SeatAllocationService;
 import org.jboss.jdf.example.ticketmonster.util.MultivaluedHashMap;
 import org.jboss.jdf.ticketmonster.test.TicketMonsterDeployment;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -20,20 +19,11 @@ public class RESTDeployment {
         return TicketMonsterDeployment.deployment()
                 .addPackage(Booking.class.getPackage())
                 .addPackage(BaseEntityService.class.getPackage())
+                .addPackage(MultivaluedHashMap.class.getPackage())
+                .addPackage(SeatAllocationService.class.getPackage())
                 .addPackage(VenueDTO.class.getPackage())
                 .addPackage(SearchService.class.getPackage())
                 .addPackage(PriceMinBridge.class.getPackage())
-                .addPackage(MultivaluedHashMap.class.getPackage())
-                .addClass(CartStore.class)
-                .addClass(CartService.class)
-                .addClass(SectionAllocationKey.class)
-                .addClass(SeatAllocationService.class)
-                .addClass(AllocatedSeats.class)
-                .addClass(MediaPath.class)
-                .addClass(MediaManager.class)
-                .addClass(Bootstrap.class)
-                .addClass(BotService.class)
-                .addClass(Bot.class)
                 .addAsLibraries(Maven.resolver()
                         .loadPomFromFile("pom.xml")
                         .resolve("org.infinispan:infinispan-core").withTransitivity().asFile());
