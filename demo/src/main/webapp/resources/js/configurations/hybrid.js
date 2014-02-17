04b90f889e161bc5ce057a61aa900e70b98e4ec5
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2014-02-18 00:39:24 +0530

    JDF-634 Added Statusbar plugin to configure statusbar on IOS7
    The StatusBar plugin and the Device plugin is used to determine
    whether to disable overlaying the statusbar on the app, and what
    background color to use for the statusbar.

--- demo/src/main/webapp/resources/js/configurations/hybrid.js
+++ demo/src/main/webapp/resources/js/configurations/hybrid.js
@@ -5,7 +5,19 @@ var TicketMonster = {
     }
 };
 
-require (["mobile"], function() {
-
-});
+(function checkCordova(){
+    // Detect if Cordova is loaded, else repeat
+    if(window.cordova) {
+        // Detect if iOS 7 or higher and disable overlaying the status bar
+        if(window.device.platform.toLowerCase() == "ios" &&
+            parseFloat(window.device.version) >= 7.0) {
+            StatusBar.overlaysWebView(false);
+            StatusBar.backgroundColorByHexString("#3C3C3C");
+        }
+        // Load the mobile module
+        require (["mobile"]);
+    } else {
+        setTimeout(checkCordova, 10);
+    }
+})();
 
